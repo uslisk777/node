805edf2fffcfaaf0cfc68567c5bdb87bc70def66
uslisk777 <uslisk777@gmail.com>
2019-07-17 21:51:20 +0300

    3

--- node_react/node-basic/app.js
+++ node_react/node-basic/app.js
@@ -1,6 +1,13 @@
 
 
 const helpers = require ("./helpers")
+const http = require ("http")
+
+const server = http.createServer((req, res) => {
+  res.end("hello world from node js")
+})
+
+server.listen(3000);
 
 const total = helpers.sum(10, 200);
 console.log("TOTAL: ", total);
