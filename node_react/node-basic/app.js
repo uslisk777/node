60309cec91175928191aa1849469a90119b5030e
uslisk777 <uslisk777@gmail.com>
2019-07-17 16:25:07 +0300

    1

--- node_react/node-basic/app.js
+++ node_react/node-basic/app.js
@@ -1,6 +1,6 @@
-function sum(a,b) {
-  return a + b;
 
-}
-const total = sum(10, 200);
-console.log("TOTAL: ", total); 
+
+const helpers = require ('./helpers')
+
+const total = helpers.sum(10, 200);
+console.log("TOTAL: ", total);
