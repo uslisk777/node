3c2d866fd94f1baf6000bc6b6ebe01ad645e7b9f
uslisk777 <uslisk777@gmil.com>
2019-08-02 14:04:18 +0000

    222

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ var express = require('express');
 var app = express();
 app.get('/', function(req, res){
    res.send("Hello World!");
+ 
 });
 app.listen(5000, '172.31.39.248');
---
+++ test.js
@@ -0,0 +1,5 @@
+var HTTPServer = require('http-server'); var httpServer = new 
+HTTPServer({
+  root: './public'
+});
+httpServer.start();
