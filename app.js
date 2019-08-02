93f4a1086db326bbf15903ed8b5b2926957d500c
uslisk777 <uslisk777@gmail.com>
2019-08-02 17:13:30 +0300

    1

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 app.get('/', function(req, res){
    res.send("Hello World!");
 
-   res.send("PROCESS: ", process);
+   res.send(process);
    console.log("PROCESS: ", process);
 });
 app.listen(5000, '172.31.39.248');
